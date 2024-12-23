56206081bc5d99a3885f14db3633fd4bcccb4587
hadad <hadad@linuxmail.org>
2024-12-23 22:52:31 +0700

    onclite: Adjust override prop key names
    Signed-off-by: Hadad <hadad@linuxmail.org>
    Change-Id: Iaa96fd777196fe90f408457a580b80d86b041140

--- lineage_onclite.mk
+++ lineage_onclite.mk
@@ -21,12 +21,11 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "xiaomi/onc/onc:9/PKQ1.181021.001/V11.0.3.0.PFLMIXM:user/release-keys"
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="onc-user 9 PKQ1.181021.001 V11.0.3.0.PFLMIXM release-keys"
+    BuildDesc="onc-user 9 PKQ1.181021.001 V11.0.3.0.PFLMIXM release-keys" \
+    BuildFingerprint=xiaomi/onc/onc:9/PKQ1.181021.001/V11.0.3.0.PFLMIXM:user/release-keys
 
 PRODUCT_PRODUCT_PROPERTIES += \
-ro.build.fingerprint=$(BUILD_FINGERPRINT)
+ro.build.fingerprint=$(BuildFingerprint)
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
